ee6b625bf45baa6f2a548481032ca7a093ade119
alvin kurniawan <alvinkurniawan2708@gmail.com>
2021-09-27 09:30:58 +0700

    Authentication

--- src/components/general/NavBar.js
+++ src/components/general/NavBar.js
@@ -1,24 +1,46 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import {connect} from 'react-redux';
+import { Avatar } from "antd";
+import { logout } from '../../actions/authAction';
 // import './script';
 
-const NavBar = () =>{
-    return (
-      <section>
-        <header>
-        <Link className="logo" to="/">HillTopShop</Link>
+const NavBar = ({auth:{isAuthenticated},logout}) =>{
+  const user = (
+    <ul>
+      <li>
         <nav className="navbar">
-          <Link to="/">Home</Link>
-          <Link to="#gallery">Shop</Link>
-          <Link to="#">Review</Link>
+        <Link className="logo" to="/">HillTopShop</Link>
+        <Link to="/">Home</Link>
+        <Link to="#gallery">Shop</Link>
+        <Link to="#"> Review</Link> 
+        <i className="fas fa-search" id="search-icon"></i>
+        <Link className="fas fa-shopping-cart" to="/cart"></Link>
+        <Link onClick={logout}to="#!">Logout</Link>
         </nav>
-        <div className="icons">
-          <i className="fas fa-bars" id="menu-bars"></i>
-          <i className="fas fa-search" id="search-icon"></i>
-          <Link className="fas fa-shopping-cart" to="/cart"></Link>
-          <i className="fas fa-user" id="user-icon"></i>
-        </div>
+      </li>
+    </ul>
+  )
+  const guest = (
+    <ul>
+    <li>
+      <nav className="navbar">
+      <Link className="logo" to="/">HillTopShop</Link>
+      <Link to="/">Home</Link>
+      <Link to="#gallery">Shop</Link>
+      <Link to="#"> Review</Link> 
+      <i className="fas fa-search" id="search-icon"></i>
+      <Link className="fas fa-shopping-cart" to="/cart"></Link>
+      <Link className="fas fa-user" id="user-icon" to="/login"></Link>
+      </nav>
+    </li>
+  </ul>
+  )
+    return (
+      <section className="navbarSect">
+        <header>
+          
+          {isAuthenticated ? user : guest}
         </header>
       </section>
     );
@@ -28,4 +50,4 @@ const mapStateToProps = state =>({
     auth:state.auth,
 });
 
-export default connect(mapStateToProps)(NavBar);
+export default connect(mapStateToProps,{logout})(NavBar);
